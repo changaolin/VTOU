ab261bf02c6f660ace70fb8832bf1b50db0bb6fc
calbook <451101751@qq.com>
2018-12-23 15:09:30 +0800

    add news_sohusite_xml.dat 百度链接

--- News/split.sh
+++ News/split.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "news_sohusite_xml.dat 文件在百度云盘，地址见README"
 inp="news_sohusite_xml.dat"
 cat $inp | iconv -f gbk -t utf-8 -c | grep "<content>"  > content.txt
 cat $inp | iconv -f gbk -t utf-8 -c | grep "<url>"  > url.txt
